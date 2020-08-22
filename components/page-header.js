0a0f6cb1b88e62b029f60cb51d147e7e1bf546c3
cresia <kimkim4@gmail.com>
2020-08-22 13:11:38 -0700

    Completed user-can-view-the-average-grade

---
+++ components/page-header.js
@@ -0,0 +1,27 @@
+class PageHeader {
+  constructor(headerElement) {
+    this.headerElement = headerElement;
+  }
+
+  updateAverage(newAverage) {
+    console.log("newAverage", newAverage);
+
+    // use this when in the main.js .querySelector is header bc it needs to search for descendents
+    // e.g: header -> span -> avgResult
+    var avgGrades = this.headerElement.querySelector('span');
+
+    var createDivAvg = document.createElement("div");
+    createDivAvg.textContent = newAverage;
+
+    avgGrades.append(createDivAvg);
+
+
+    // ------- or by using the tag element directly or by class or id on main.js -------
+    //if using the .querySelector("span") directly on main.js, does not need line 11 (comment it out) because
+    // it's already on span -> avgResult
+    //also comment out line 16
+    //this way can use this.headerElement directly to append since this.headerEleemnt is span element
+    // this.headerElement.append(createDivAvg);
+
+  }
+}
